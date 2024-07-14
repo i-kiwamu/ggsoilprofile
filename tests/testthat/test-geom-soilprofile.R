532119432357110d867cde5c2abe1aa3fc18e6dd
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2024-07-14 21:51:47 +0900

    change test

--- tests/testthat/test-geom-soilprofile.R
+++ tests/testthat/test-geom-soilprofile.R
@@ -1,12 +1,10 @@
 library(ggplot2)
 
 test_that("multiplication works", {
-  fig1 <- pedons %>%
-    ggplot(aes(pedon, top, yend = bottom, group = paste(pedon, top, bottom))) +
+  fig1 <- ggplot(pedons, aes(pedon, top = top, bottom = bottom, group = paste(pedon, top, bottom))) +
     geom_horizon(aes(fill = color)) +
     scale_x_discrete(position = "top") +
-    scale_y_reverse("Depth (cm)") +
-    scale_fill_manual(values = mnsl2hex(sort(unique(pedons$color))),
-                      na.value = "transparent", guide = "none")
+    ylab("Depth (cm)") +
+    coord_trans(y = "reverse")
   expect_no_error(fig1)
 })
