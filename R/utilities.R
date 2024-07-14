ef83ef25174bc42cd48ecb42ddeff330193aa366
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2024-07-14 21:57:28 +0900

    Add roxygen for mnsl2hex

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,8 @@
+#' mnsl2hex
+#' @description Convert from Munsell color to RGB hex.
+#' @param mnsl character of Munsell color (ex 10YR 4/2, N 3/0)
+#' @return character of RGB hex (ex #6D5D4A, #474747)
+#' @export
 mnsl2hex <- function(mnsl) {
   mnsl <- ifelse(is.na(mnsl), "N 0/0", mnsl)
   df_rgb <- munsellinterpol::MunsellToRGB(mnsl)$RGB
