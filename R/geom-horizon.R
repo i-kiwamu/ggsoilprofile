d9653c217aa51995c0c544251d6244f15eed4144
ISHIKURA Kiwamu <ishikura.kiwamu@gmail.com>
2024-07-15 11:59:38 +0900

    add boundary type

--- R/geom-horizon.R
+++ R/geom-horizon.R
@@ -27,21 +27,13 @@ GeomPedonLine <- ggplot2::ggproto(
                             yend = bottom)
     data_top <- data %>%
       group_by(x) %>%
-      filter(top == max(top)) %>%
+      filter(top == min(top)) %>%
       ungroup() %>%
       transform(x = x - width * just,
                 xend = x + width * (1 - just),
                 y = top,
                 yend = top)
-    data_inside <- data %>%
-      group_by(x) %>%
-      filter(top != max(top)) %>%
-      ungroup() %>%
-      transform(x = x - width * just,
-                xend = x + width * (1 - just),
-                y = top,
-                yend = top)
-    ggplot2::flip_data(rbind(data_left, data_right, data_top, data_inside),
+    ggplot2::flip_data(rbind(data_left, data_right, data_top),
                              params$flipped_aes)
   },
   draw_panel = ggplot2::GeomSegment$draw_panel,
@@ -62,7 +54,59 @@ geom_pedon_line <-
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
-}
+  }
+
+
+#' GeomBoundary
+#' @description A \code{ggproto} object for boundary lines between horizons.
+#' @importFrom dplyr group_by filter ungroup
+GeomBoundary <- ggplot2::ggproto(
+  "GeomBoundary", ggplot2::Geom,
+  required_aes = c("x", "top", "bottom"),
+  default_aes = ggplot2::aes(boundary_type = NULL,
+                             linetype = "solid", linewidth = 0.5,
+                             colour = "black", alpha = NA),
+  setup_data = function(data, params) {
+    data$flipped_aes <- params$flipped_aes
+    data <- ggplot2::flip_data(data, params$flipped_aes)
+    data$width <- data$width %||%
+      params$width %||%
+      (min(vapply(split(data$x, data$PANEL, drop = TRUE),
+                  ggplot2::resolution,
+                  numeric(1), zero = FALSE)) * 0.9)
+    data$just <- params$just %||% 0.5
+    data <- data %>%
+      group_by(x) %>%
+      filter(top != max(top)) %>%
+      ungroup() %>%
+      transform(x = x - width * just,
+                xend = x + width * (1 - just),
+                y = bottom,
+                yend = bottom,
+                linetype = ifelse(is.na(boundary_type), "solid", boundary_type),
+                linewidth = ifelse(is.na(boundary_type), 0.3, boundary_type))
+    ggplot2::flip_data(data,
+                       params$flipped_aes)
+  },
+  draw_panel = ggplot2::GeomSegment$draw_panel,
+  draw_key = ggplot2::draw_key_path,
+  rename_size = TRUE
+)
+
+
+#' geom_boundary
+#' @description A \code{geom} object for boundary line between horizons.
+#' @inheritParams ggplot2::geom_segment
+geom_boundary <-
+  function(mapping = NULL, data = NULL, stat = "identity",
+           position = "identity", na.rm = FALSE, show.legend = NA,
+           inherit.aes = TRUE, ...) {
+    ggplot2::layer(
+      geom = GeomBoundary, mapping = mapping, data = data, stat = stat,
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    )
+  }
 
 
 #' GeomHorizonMunsell
@@ -108,7 +152,7 @@ geom_horizon_munsell <-
 
 
 pattern_aesthetics <- ggplot2::aes(
-  pattern                  = 'stripe',
+  pattern                  = "none",
   pattern_type             = NA,
   pattern_subtype          = NA,
 
@@ -214,9 +258,22 @@ geom_horizon <- function(mapping = NULL, data = NULL, stat = "identity",
          geom_pedon_line(
            mapping = mapping[names(mapping) != "fill"],
            data = data, stat = stat, position = "identity", na.rm = na.rm,
-           show.legend = show.legend, inherit.aes = inherit.aes, ...))
+           show.legend = show.legend, inherit.aes = inherit.aes, ...),
+         geom_boundary(
+           mapping = mapping[names(mapping) != "fill"],
+           data = data, stat = stat, position = "identity", na.rm = na.rm,
+           show.legend = show.legend, inherit.aes = inherit.aes, ...),
+         ggplot2::scale_linetype_manual(
+           values = c("very abrupt" = "solid", "abrupt" = "solid", "clear" = "dotdash",
+                      "gradual" = "dotted"),
+           na.value = "solid"),
+         ggplot2::scale_linewidth_manual(
+           values = c("very abrupt" = 1, "abrupt" = 0.3, "clear" = 0.3,
+                      "gradual" = 0.3),
+           na.value = 0.3))
   } else if ("pattern" %in% names(mapping)) {
-    list(geom_horizon_humus(mapping = mapping, data = data, stat = stat,
+    list(geom_horizon_humus(mapping = mapping[names(mapping) != "boundary_type"],
+                            data = data, stat = stat,
                             position = ggplot2::position_stack(reverse = TRUE),
                             ...,
                             na.rm = na.rm, show.legend = show.legend,
@@ -228,8 +285,20 @@ geom_horizon <- function(mapping = NULL, data = NULL, stat = "identity",
            values = c("little" = "blank", "common" = "dotted",
                       "rich" = "solid", "very rich" = "solid")),
          geom_pedon_line(
+           mapping = mapping[setdiff(names(mapping), c("boundary_type", "pattern", "pattern_linetype"))],
+           data = data, stat = stat, position = "identity", na.rm = na.rm,
+           show.legend = show.legend, inherit.aes = inherit.aes, ...),
+         geom_boundary(
            mapping = mapping[setdiff(names(mapping), c("pattern", "pattern_linetype"))],
            data = data, stat = stat, position = "identity", na.rm = na.rm,
-           show.legend = show.legend, inherit.aes = inherit.aes, ...))
+           show.legend = show.legend, inherit.aes = inherit.aes, ...),
+         ggplot2::scale_linetype_manual(
+           values = c("very abrupt" = "solid", "abrupt" = "solid", "clear" = "dotdash",
+                      "gradual" = "dotted"),
+           na.value = "solid"),
+         ggplot2::scale_linewidth_manual(
+           values = c("very abrupt" = 1, "abrupt" = 0.3, "clear" = 0.3,
+                      "gradual" = 0.3),
+           na.value = 0.3))
   }
 }
